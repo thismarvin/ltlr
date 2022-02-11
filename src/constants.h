e1928c8f9fd2576b626931b2ab189f9075f84f26
marvin <marvin@thismarvin.com>
2022-02-10 23:07:27 -0600

    feat: added Vector2 utility functions/macros
    Co-authored-by: Austin Aurelio <austin@delveam.com>

--- src/constants.h
+++ src/constants.h
@@ -3,6 +3,17 @@
 
 #include "raylib.h"
 
+#define VECTOR2_ZERO (Vector2){ .x = 0, .y = 0 }
+
+inline Vector2 Vector2Create(float x, float y)
+{
+    return (Vector2)
+    {
+        .x = x,
+        .y = y,
+    };
+}
+
 #define MAX(a,b) (((a)>(b))?(a):(b))
 #define MIN(a,b) (((a)<(b))?(a):(b))
 
