13d0a96a2fa3d3eebf836d85ef37d1fe04464a44
marvin <marvin@thismarvin.com>
2022-02-10 21:27:42 -0600

    fix: initialize Polygon's length properties
    Co-authored-by: Austin Aurelio <austin@delveam.com>

--- src/polygon.c
+++ src/polygon.c
@@ -5,6 +5,8 @@ void PolygonInit(Polygon* self, size_t verticesLength)
 {
     self->vertices = calloc(verticesLength, sizeof(Vector2));
     self->edges = calloc(verticesLength, sizeof(LineSegment));
+    self->verticesLength = verticesLength;
+    self->edgesLength = verticesLength;
 }
 
 void PolygonDestroy(Polygon* self)
