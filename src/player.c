a28a091689099712bffb86cd1fb73ffdd5d7fa7c
marvin <marvin@thismarvin.com>
2022-02-15 23:22:01 -0600

    wip: incorporated sat based collision
    This is really just a proof of concept. Blocks should definitely not be
    stored in `Player.c`!
    
    With that said, our Player controller finally supports collisions with
    convex polygons.
    
    Co-authored-by: Austin Aurelio <austin@delveam.com>

--- src/player.c
+++ src/player.c
@@ -1,8 +1,12 @@
+#include "collider.h"
 #include "constants.h"
 #include "context.h"
 #include "player.h"
 #include "polygon.h"
 #include "raymath.h"
+#include "sat.h"
+
+static Rectangle blocks[4];
 
 static void PlayerInput(Player* self)
 {
@@ -53,6 +57,24 @@ static void PlayerCollision(Player* self)
         self->jumping = 0;
         self->grounded = 1;
     }
+
+    // TODO(thismarvin): Support arbitrary convex polygons!
+    // Polygon playerPolygon = PolygonFromRectangle(self->aabb);
+
+    for (int i = 0; i < 4; ++i)
+    {
+        Vector2 resolution = RectangleRectangleResolution(self->aabb, blocks[i]);
+
+        PlayerTranslate(self, resolution);
+
+        if (resolution.y < 0) {
+            self->velocity.y = 0;
+            self->jumping = 0;
+            self->grounded = 1;
+        }
+    }
+
+    // PolygonDestroy(&playerPolygon);
 }
 
 void PlayerInit(Player* self, Vector2 position)
@@ -69,6 +91,16 @@ void PlayerInit(Player* self, Vector2 position)
     self->acceleration = Vector2Create(0, 1000);
     self->jumping = 0;
     self->grounded = 0;
+
+    blocks[0] = (Rectangle) {
+        200, 180 - 120, 64, 32
+    };
+    blocks[1] = (Rectangle) {
+        200, 180 - 32 - 4, 32, 32
+    };
+    blocks[2] = (Rectangle) {
+        200 + 64, 180 - 32 - 4, 32, 32
+    };
 }
 
 void PlayerSetPos(Player* self, Vector2 position)
@@ -96,4 +128,8 @@ void PlayerUpdate(Player* self)
 void PlayerDraw(Player* self)
 {
     DrawRectangleRec(self->aabb, RED);
+
+    for (int i = 0; i < 4; ++i) {
+        DrawRectangleLinesEx(blocks[i], 4, BLACK);
+    }
 }
