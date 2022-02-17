acf127aeb4dcf2179a5dce400ae03debe209f0e6
marvin <marvin@thismarvin.com>
2022-02-16 23:24:21 -0600

    feat: first pass at `Kinetic` struct's implementation
    This new `Kinetic` struct abstracts away things like physics integration
    and rendering interpolation. The API is still a little wonky, but we are
    getting there!
    
    Co-authored-by: Austin Aurelio <austin@delveam.com>

---
+++ src/kinetic.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "raylib.h"
+
+typedef struct {
+    Vector2 previousPosition;
+    Vector2 currentPosition;
+    Vector2 velocity;
+    Vector2 acceleration;
+    bool teleported;
+} Kinetic;
+
+Kinetic KineticCreate(Vector2 position, Vector2 velocity, Vector2 acceleration);
+void KineticIntegrate(Kinetic* self);
+void KineticTeleport(Kinetic* self, Vector2 position);
+Vector2 KineticGetRenderPosition(Kinetic* self);
