8a3ed0f3c65d52557e2bff4001381f43c521d1c3
marvin <marvin@thismarvin.com>
2022-02-18 12:30:15 -0600

    feat: added rust inspired numeric `typedef`s
    Rust's naming convention for numeric types (e.g. i32, u8, etc.) is much
    more ergonomic than C's (e.g. int32_t, uint8_t, etc.). This commit
    includes a bunch of typdefs that mimic Rust's naming convention. This is
    probably a bad idea, but we can always remove them later!

--- src/common.h
+++ src/common.h
@@ -1,6 +1,20 @@
 #pragma once
 
 #include "raylib.h"
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef int8_t i8;
+typedef uint8_t u8;
+typedef int16_t i16;
+typedef uint16_t u16;
+typedef int32_t i32;
+typedef uint32_t u32;
+typedef int64_t i64;
+typedef uint64_t u64;
+typedef size_t usize;
+typedef float f32;
+typedef double f64;
 
 #define MAX(a,b) (((a)>(b))?(a):(b))
 #define MIN(a,b) (((a)<(b))?(a):(b))
