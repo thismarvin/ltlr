6e70e67c1a592b94a9ca00d7609554c0e9277b98
marvin <me@thismarvin.com>
2022-04-12 21:46:57 -0500

    feat: implement our own basic test suite
    Co-authored-by: Austin Aurelio <austin@delveam.com>

---
+++ src/testing.c
@@ -0,0 +1,71 @@
+#include "testing.h"
+#include <stdio.h>
+
+const char* evalPrompt = "\x1b[43m\x1b[30m EVAL \x1b[0m";
+const char* passPrompt = "\x1b[42m\x1b[30m PASS \x1b[0m";
+const char* failPrompt = "\x1b[41m\x1b[30m FAIL \x1b[0m";
+const char* donePassPrompt = "\x1b[42m\x1b[30m DONE \x1b[0m";
+const char* doneFailPrompt = "\x1b[41m\x1b[30m DONE \x1b[0m";
+
+TestSuite TestSuiteCreate(const char* name)
+{
+    return (TestSuite)
+    {
+        .name = (char*)name,
+        .records = DEQUE_OF(TestRecord),
+        .passed = 0,
+        .failed = 0,
+    };
+}
+
+void TestSuiteAdd(TestSuite* self, const char* name, TestFn fn)
+{
+    bool passed = fn();
+
+    TestRecord record = (TestRecord)
+    {
+        .name = (char*)name,
+        .passed = passed,
+    };
+
+    DequePushFront(&self->records, &record);
+
+    if (passed)
+    {
+        self->passed += 1;
+    }
+    else
+    {
+        self->failed += 1;
+    }
+}
+
+void TestSuitePresentResults(TestSuite* self)
+{
+    printf("%s %s\n", evalPrompt, self->name);
+
+    for (usize i = 0; i < DequeGetSize(&self->records); ++i)
+    {
+        TestRecord record = DEQUE_GET_UNCHECKED(&self->records, TestRecord, i);
+
+        if (record.passed)
+        {
+            printf("%s %s\n", passPrompt, record.name);
+        }
+        else
+        {
+            printf("%s %s\n", failPrompt, record.name);
+        }
+    }
+
+    if (self->failed == 0)
+    {
+        printf("%s %lu passed; %lu failed\n", donePassPrompt, self->passed, self->failed);
+    }
+    else
+    {
+        printf("%s %lu passed; %lu failed\n", doneFailPrompt, self->passed, self->failed);
+    }
+
+    DequeDestroy(&self->records);
+}
---
+++ src/testing.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "common.h"
+#include "deque.h"
+
+typedef bool (*TestFn)(void);
+
+typedef struct
+{
+    char* name;
+    bool passed;
+} TestRecord;
+
+typedef struct
+{
+    char* name;
+    Deque records;
+    usize passed;
+    usize failed;
+} TestSuite;
+
+TestSuite TestSuiteCreate(const char* name);
+void TestSuiteAdd(TestSuite* self, const char* name, TestFn fn);
+void TestSuitePresentResults(TestSuite* self);
