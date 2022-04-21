5c299a3fa0bfb60dd363800841ab0894ff388e0e
marvin <me@thismarvin.com>
2022-04-21 18:35:18 -0500

    feat: implement an `EventHandler` struct
    The implementation is heavily inspired by the following:
    
    https://docs.microsoft.com/en-us/dotnet/api/system.eventhandler?view=net-6.0

--- src/events.c
+++ src/events.c
@@ -2,6 +2,34 @@
 #include "scene.h"
 #include <raymath.h>
 
+EventHandler EventHandlerCreate()
+{
+    return (EventHandler)
+    {
+        .listeners = DEQUE_OF(OnRaise),
+    };
+}
+
+void EventHandlerSubscribe(EventHandler* self, const OnRaise onRaise)
+{
+    DequePushFront(&self->listeners, &onRaise);
+}
+
+void EventHandlerRaise(const EventHandler* self, const void* arguments)
+{
+    for (usize i = 0; i < DequeGetSize(&self->listeners); ++i)
+    {
+        const OnRaise* onRaise = &DEQUE_GET_UNCHECKED(&self->listeners, OnRaise, i);
+
+        (*onRaise)(arguments);
+    }
+}
+
+void EventHandlerDestroy(EventHandler* self)
+{
+    DequeDestroy(&self->listeners);
+}
+
 // TODO(thismarvin): Is this too specialized?
 void RaiseSpawnCloudParticleEvent(const EventCloudParticleParams* params)
 {
--- src/events.h
+++ src/events.h
@@ -1,6 +1,19 @@
 #pragma once
 
 #include "common.h"
+#include "deque.h"
+
+typedef void (*OnRaise)(const void*);
+
+typedef struct
+{
+    Deque listeners;
+} EventHandler;
+
+EventHandler EventHandlerCreate();
+void EventHandlerSubscribe(EventHandler* self, OnRaise onRaise);
+void EventHandlerRaise(const EventHandler* self, const void* arguments);
+void EventHandlerDestroy(EventHandler* self);
 
 typedef struct Scene Scene;
 
