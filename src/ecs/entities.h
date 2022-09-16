5e7656965070da81f72c5cf3fbfd1402123bdd95
Austin Aurelio <austin@delveam.com>
2022-09-15 23:13:35 -0500

    chore: sort entity includes

--- src/ecs/entities.h
+++ src/ecs/entities.h
@@ -2,11 +2,11 @@
 
 #include "entity_builder.h"
 
+#include "./entities/battery.h"
 #include "./entities/block.h"
 #include "./entities/cloud_particle.h"
 #include "./entities/fog.h"
 #include "./entities/fog_particle.h"
 #include "./entities/player.h"
-#include "./entities/walker.h"
-#include "./entities/battery.h"
 #include "./entities/solar_panel.h"
+#include "./entities/walker.h"
