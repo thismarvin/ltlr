d5fcac9d4e9199fbcdfcebbf01b750472b44d459
Marvin Valenzuela <me@thismarvin.com>
2022-11-08 08:14:33 -0600

    chore: bump version number
    I am not sure if this is a good idea or not, but I think the version
    number should just be the current date...

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   outputs = { self, nixpkgs }:
     let
       pname = "ltlr";
-      version = "0.1.0";
+      version = "2022-11-07";
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
     in
