807b54234a4ebab88be9a67209f7bac678753461
Marvin Valenzuela <me@thismarvin.com>
2022-11-21 12:12:30 -0600

    chore: bump version number

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   outputs = { self, nixpkgs }:
     let
       pname = "ltlr";
-      version = "2022-11-14";
+      version = "2022-11-21";
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
     in
