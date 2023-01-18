fd92d9354f2e5550bba5e22648ca507bfe1b683b
Marvin Valenzuela <me@thismarvin.com>
2023-01-17 21:06:43 -0600

    wip: implement an experimental `Easer` struct

--- src/easing.c
+++ src/easing.c
@@ -31,3 +31,55 @@ f64 EaseInOutQuad(const EasingFnParams* params)
 
     return x < 0.5 ? 2.0 * x * x : 1.0 - pow(-2.0 * x + 2.0, 2.0) / 2.0;
 }
+
+Easer EaserCreate(const EasingFn ease, const f64 duration)
+{
+    return (Easer)
+    {
+        .elapsed = 0.0,
+        .duration = duration,
+        .value = 0.0,
+        .ease = ease,
+    };
+}
+
+static void EaserRecalculateValue(Easer* self)
+{
+    const f64 value = self->elapsed / self->duration;
+
+    const EasingFnParams params = (EasingFnParams)
+    {
+        .value = value,
+    };
+
+    self->value = self->ease(&params);
+}
+
+bool EaserIsDone(const Easer* self)
+{
+    return self->elapsed >= self->duration;
+}
+
+void EaserUpdate(Easer* self, f64 deltaTime)
+{
+    self->elapsed += deltaTime;
+
+    EaserRecalculateValue(self);
+}
+
+f64 EaserLerp(const Easer* self, const f64 start, const f64 end)
+{
+    return start + self->value * (end - start);
+}
+
+f64 EaserLerpPrecise(const Easer* self, const f64 start, const f64 end)
+{
+    return (1.0 - self->value) * start + self->value * end;
+}
+
+void EaserReset(Easer* self)
+{
+    self->elapsed = 0.0;
+
+    EaserRecalculateValue(self);
+}
--- src/easing.h
+++ src/easing.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdbool.h>
+
 typedef double f64;
 
 typedef struct
@@ -14,3 +16,18 @@ f64 EaseLinear(const EasingFnParams* params);
 f64 EaseInQuad(const EasingFnParams* params);
 f64 EaseOutQuad(const EasingFnParams* params);
 f64 EaseInOutQuad(const EasingFnParams* params);
+
+typedef struct
+{
+    f64 elapsed;
+    f64 duration;
+    f64 value;
+    EasingFn ease;
+} Easer;
+
+Easer EaserCreate(const EasingFn ease, f64 duration);
+bool EaserIsDone(const Easer* self);
+void EaserUpdate(Easer* self, f64 deltaTime);
+f64 EaserLerp(const Easer* self, f64 start, f64 end);
+f64 EaserLerpPrecise(const Easer* self, f64 start, f64 end);
+void EaserReset(Easer* self);
