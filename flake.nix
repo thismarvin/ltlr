492302c74e44d2c16a57005a0c4442d9d04a0e18
Marvin Valenzuela <me@thismarvin.com>
2023-01-23 12:03:39 -0600

    chore: setup and use nix formatter

--- flake.nix
+++ flake.nix
@@ -3,60 +3,61 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs }:
-    let
-      pname = "ltlr";
-      version = "2023-01-16";
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
-      packages."${system}" = {
-        ltlr = derivation {
-          name = "${pname}-${version}";
-          inherit pname;
-          inherit version;
-          src = ./.;
-          inherit system;
-          builder = "${pkgs.nushell}/bin/nu";
-          args = [ ./builder.nu ];
-          buildInputs = with pkgs; [
-            xorg.libX11.dev
-            xorg.libXcursor.dev
-            xorg.libXext.dev
-            xorg.libXfixes.dev
-            xorg.libXi.dev
-            xorg.libXinerama.dev
-            xorg.libXrandr.dev
-            xorg.libXrender.dev
-            xorg.xorgproto
-            libGL.dev
-            coreutils
-            binutils-unwrapped
-            nushell
-            pkg-config
-            gcc
-            patchelf
-            gnumake
-          ];
-        };
-        default = self.packages."${system}".ltlr;
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pname = "ltlr";
+    version = "2023-01-16";
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    formatter."${system}" = pkgs.alejandra;
+    packages."${system}" = {
+      ltlr = derivation {
+        name = "${pname}-${version}";
+        inherit pname;
+        inherit version;
+        src = ./.;
+        inherit system;
+        builder = "${pkgs.nushell}/bin/nu";
+        args = [./builder.nu];
+        buildInputs = with pkgs; [
+          xorg.libX11.dev
+          xorg.libXcursor.dev
+          xorg.libXext.dev
+          xorg.libXfixes.dev
+          xorg.libXi.dev
+          xorg.libXinerama.dev
+          xorg.libXrandr.dev
+          xorg.libXrender.dev
+          xorg.xorgproto
+          libGL.dev
+          coreutils
+          binutils-unwrapped
+          nushell
+          pkg-config
+          gcc
+          patchelf
+          gnumake
+        ];
       };
-
-      devShells."${system}" = {
-        minimal = pkgs.mkShell {
-          inputsFrom = [ self.packages."${system}".default ];
-          packages = with pkgs; [
-            astyle
-          ];
-        };
-        extra = pkgs.mkShell {
-          inputsFrom = [ self.devShells."${system}".minimal ];
-          packages = with pkgs; [
-            emscripten
-          ];
-        };
-        default = self.devShells."${system}".minimal;
-      };
+      default = self.packages."${system}".ltlr;
     };
+    devShells."${system}" = {
+      minimal = pkgs.mkShell {
+        inputsFrom = [self.packages."${system}".default];
+        packages = with pkgs; [
+          astyle
+        ];
+      };
+      extra = pkgs.mkShell {
+        inputsFrom = [self.devShells."${system}".minimal];
+        packages = with pkgs; [
+          emscripten
+        ];
+      };
+      default = self.devShells."${system}".minimal;
+    };
+  };
 }
